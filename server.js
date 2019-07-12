97153d08a9d0ae31bb8943d2edf2c0b449232416
Ty Lippe <tylippe94@gmail.com>
2019-07-12 11:23:21 -0400

    npm run server working

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const server = express();
+const helmet = require('helmet');
+const actionRouter = require("./actions/actionRouter")
+const projectRouter = require("./projects/projectRouter")
+
+server.use(express.json());
+server.use(helmet());
+server.use(logger);
+
+server.use("/api/actions", actionRouter);
+server.use("/api/project", projectRouter);
+
+
+server.get('/', logger, (req, res) => {
+  res.send(`<h2>SPRINT DAY!</h2>`)
+});
+
+
+function logger(req, res, next) {
+  console.log(`${req.method} Request`);
+  next();
+}
+
+module.exports = server;
